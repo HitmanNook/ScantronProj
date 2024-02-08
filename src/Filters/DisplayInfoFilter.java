59f7a18d9c93851f51de791a6cc5170b23249b22
HitmanNook <119698304+HitmanNook@users.noreply.github.com>
2024-02-08 13:02:46 -0800

    Crop method

--- src/Filters/DisplayInfoFilter.java
+++ src/Filters/DisplayInfoFilter.java
@@ -8,28 +8,42 @@ import java.sql.SQLOutput;
 import java.util.ArrayList;
 import java.util.HashMap;
 
-public class DisplayInfoFilter implements PixelFilter {
+import static java.lang.Integer.MAX_VALUE;
+
+public class DisplayInfoFilter implements PixelFilter, Interactive{
     public DisplayInfoFilter() {
         System.out.println("Filter running...");
     }
 
-    public void findMarked (DImage img) {
-        short[][] grid = img.getBWPixelGrid();
-        grid = crop(grid, 0, 0, 500, 500);
+    public void findMarked(short[][] grid) {
+        //grid = crop(grid, 0, 0, 500, 500);
 
         HashMap<String,Integer> options = new HashMap<>();
 
         options.put("a", 106);
         options.put("b", 131);
         options.put("c", 156);
-        options.put("d", 180);
+        options.put("d", 178);
         options.put("e", 204);
-
+        String lowestAvgOption = "";
+        int lowestAvg = MAX_VALUE;
         for(String s : options.keySet()){
-
+            System.out.println(s);
+            int avg = 0;
+            for(int r = options.get(s); r < options.get(s) + 20; r++){
+                for(int c = 109; c < 129; c++){
+                    avg += grid[r][c];
+                }
+            }
+            avg = avg / 400;
+            System.out.println(s + " has " + avg + " marked");
+            if(avg < lowestAvg){
+                lowestAvg = avg;
+                System.out.println(s);
+                lowestAvgOption = s;
+            }
         }
-
-
+        System.out.println("The most marked option is " + lowestAvgOption);
     }
 
     @Override
@@ -42,7 +56,7 @@ public class DisplayInfoFilter implements PixelFilter {
         int whiteCount = 0;
         for (int r = 0; r < grid.length; r++) {
             for (int c = 0; c < grid[0].length; c++) {
-                if (grid[r][c] < 10) blackCount++;
+                if (grid[r][c] < 100) blackCount++;
                 if (grid[r][c] > 240) whiteCount++;
             }
         }
@@ -50,6 +64,7 @@ public class DisplayInfoFilter implements PixelFilter {
         System.out.println(blackCount + " nearly black pixels and " + whiteCount + " nearly white pixels");
         System.out.println("----------------------------------------");
         System.out.println("If you want, you could output information to a file instead of printing it.");
+        findMarked(grid);
         img.setPixels(grid);
         return img;
     }
@@ -65,5 +80,16 @@ public class DisplayInfoFilter implements PixelFilter {
     }
 
 
+    @Override
+    public void mouseClicked(int mouseX, int mouseY, DImage img) {
+        short [][] grid = img.getBWPixelGrid();
+        grid = crop(grid, 0, 0, 500, 500);
+        System.out.println("Mouse clicked at " + mouseX + ", " + mouseY + " with pixel value " + grid[mouseY][mouseX]);
+    }
+
+    @Override
+    public void keyPressed(char key) {
+
+    }
 }
 
